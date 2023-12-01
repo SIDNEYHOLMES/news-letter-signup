75a94d8334fba0e7875448a1bff08fe0ae240cc0
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2023-12-01 03:50:24 -0600

    added signup check styling for fourm

--- news-letter-fourm/src/components/Fourm.tsx
+++ news-letter-fourm/src/components/Fourm.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import Image from 'next/image'
+import listCheck from '../../public/list-check-icon.svg'
 
 const Fourm = () => {
   return (
-    <main className='flex flex-col h-full p-10'>
-      <h1 className='text-6xl'>Stay Updated</h1>
-      <p>Join 60,000+ product managers reciving monthy updates on:</p>
-      <ul>
-        <li>Product discovery and building what matters</li>
-        <li>Measuring to ensure updates are a success</li>
-        <li>And much more!</li>
-        <form>
+    <main className='flex flex-col h-full p-12'>
+      <h1 className='text-6xl font-bold text-indigo-900 pt-12'>Stay updated!</h1>
+      <p className='py-8'>Join 60,000+ product managers reciving monthy updates on:</p>
+      <ul className='space-y-5 '>
+        <li className='flex items-center'>
+        <Image
+        className='mx-4'
+        priority
+        src={listCheck}
+        alt='login-check-icon'/>
+          <p>Product discovery and building what matters</p>
+        </li>
+        <li className='flex items-center'>
+          <Image
+          className='mx-4'
+          priority
+          src={listCheck}
+          alt='login-check-icon'/>
+          <p>Measuring to ensure updates are a success</p>
+        </li>
+        <li className='flex items-center'>
+          <Image
+          className='mx-4'
+          priority
+          src={listCheck}
+          alt='login-check-icon'/>
+          <p>And much more!</p>
+        </li>
+        <form className='pt-12'>
           <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900'>Email address</label>
           <input type='email' id='email' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'></input>
           <button>Subscribe to monthly newsletter</button>
